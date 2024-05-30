f3cc77b3e02100d8b6a30c9e677a95a302a49184
sanixto <sanix.to@gmail.com>
2024-05-30 21:31:28 +0300

    render movies for each page

--- src/app/movies/page.tsx
+++ src/app/movies/page.tsx
@@ -1,11 +1,24 @@
 import MoviesGrid from '@/components/movies-grid/movies-grid';
-import { getMovies } from '@/lib/movies';
+import Pagination from '@/components/pagination';
+import { MOVIES_PER_PAGE } from '@/constants';
+import { getMovies, getMoviesLength } from '@/lib/movies';
 
-export default async function Home() {
-  const movies = await getMovies();
+interface MoviesPageProps {
+  searchParams: {
+    page?: string,
+  }
+}
+
+export default async function MoviesPage({ searchParams }: MoviesPageProps) {
+  const currentPage = Number(searchParams?.page) || 1;
+  const movies = await getMovies(currentPage, MOVIES_PER_PAGE);
+  const moviesLengh = await getMoviesLength();
+  const totalPages = moviesLengh / MOVIES_PER_PAGE;
+
   return (
     <main className="p-4 md:p-10 lg:p-20">
       <MoviesGrid movies={movies} />
+      <Pagination totalPages={totalPages} />
     </main>
   );
 }
--- src/lib/movies.ts
+++ src/lib/movies.ts
@@ -1,7 +1,8 @@
 import Movie from '@/interfaces/movie.interface';
 import { sql } from '@vercel/postgres';
 
-export async function getMovies() {
+export async function getMovies(page: number, limit: number): Promise<Movie[]> {
+  const offset: number = (page - 1) * limit;
   try {
     const data = await sql<Movie>`
       SELECT
@@ -16,11 +17,24 @@ export async function getMovies() {
         image_url AS "imageUrl",
         trailer_url AS "trailerUrl"
       FROM movies
+      LIMIT ${limit}
+      OFFSET ${offset}
     `;
 
     return data.rows;
   } catch (error) {
     console.error('Database Error:', error);
-    throw new Error('Failed to fetch revenue data.');
+    throw new Error('Failed to fetch movies data.');
+  }
+}
+
+export async function getMoviesLength(): Promise<number> {
+  try {
+    const data = await sql`SELECT COUNT(*) AS count FROM movies`;
+
+    return data.rows[0].count;
+  } catch (error) {
+    console.error('Database Error:', error);
+    throw new Error('Failed to fetch movies data length.');
   }
 }
