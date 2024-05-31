ee469bdb5852efd3a202d5cd7e6291447d010230
sanixto <sanix.to@gmail.com>
2024-05-31 21:14:30 +0300

    add Footer component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 
 import './globals.css';
 import Header from '@/components/header';
+import Footer from '@/components/footer';
 
 export const metadata: Metadata = {
   title: `Cinemax`,
@@ -18,6 +19,7 @@ export default function RootLayout({
       <body>
         <Header />
         {children}
+        <Footer />
       </body>
     </html>
   );
