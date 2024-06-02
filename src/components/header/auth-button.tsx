4326b700917bd6ede5688017d23e430e0b7ad293
sanixto <sanix.to@gmail.com>
2024-06-02 20:28:24 +0300

    add empty pages

--- src/components/header/auth-button.tsx
+++ src/components/header/auth-button.tsx
@@ -1,8 +1,8 @@
 'use client';
 
+import styles from './nav-link.module.css';
 import { useUser } from '@auth0/nextjs-auth0/client';
 
-import NavLink from './nav-link';
 import { LOGIN_PATH, LOGOUT_PATH } from '@/constants';
 
 export default function AuthButton() {
@@ -11,9 +11,8 @@ export default function AuthButton() {
   if (isLoading) return <div>Loading...</div>;
   if (error) throw error;
 
-  return user ? (
-    <NavLink href={LOGOUT_PATH}>Logout</NavLink>
-   ) : (
-     <NavLink href={LOGIN_PATH}>Login</NavLink>
-   );
+  const path = user ? LOGOUT_PATH : LOGIN_PATH;
+  const text = user ? 'Logout' : 'Login';
+
+  return <a className={styles.link} href={path}>{text}</a>;
 }
