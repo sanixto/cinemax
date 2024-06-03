1ed42331f8e3edd30c99ad64a475c31572879fb2
sanixto <sanix.to@gmail.com>
2024-06-03 03:35:29 +0300

    update placeholder data and seed

--- src/scripts/seed.ts
+++ src/scripts/seed.ts
@@ -1,12 +1,12 @@
-import { movies } from '../lib/placeholder-data';
+import { movies, reviews, showtimes, users } from '../lib/placeholder-data';
 import prisma from '../lib/prisma';
 
-export default async function seedMovies() {
+async function seedMovies() {
   for (const movie of movies) {
-    await prisma.movies.create({
+    await prisma.movie.create({
       data: {
         id: movie.id,
-        title: movie.title,
+        title: movie.title.toLowerCase(),
         description: movie.description,
         year: movie.year,
         duration: movie.duration,
@@ -21,9 +21,57 @@ export default async function seedMovies() {
   console.log(`Seeded ${movies.length} movies`);
 }
 
+async function seedShowtimes() {
+  for (const showtime of showtimes) {
+    const formattedDate = new Date(showtime.date.toISOString().split('T')[0]);
+    await prisma.showtime.create({
+      data: {
+        id: showtime.id,
+        movie_id: showtime.movieId,
+        date: formattedDate,
+        time: showtime.time,
+        available_seats: JSON.stringify(showtime.availableSeats),
+      },
+    });
+  }
+  console.log(`Seeded ${showtimes.length} showtimes`);
+}
+
+async function seedUsers() {
+  for (const user of users) {
+    await prisma.user.create({
+      data: {
+        id: user.id,
+        name: user.name,
+        email: user.email,
+        image_url: user.imageUrl,
+      },
+    });
+  }
+  console.log(`Seeded ${users.length} users`);
+}
+
+async function seedReviews() {
+  for (const review of reviews) {
+    await prisma.review.create({
+      data: {
+        id: review.id,
+        user_id: review.userId,
+        movie_id: review.movieId,
+        rating: review.rating,
+        comment: review.comment,
+      },
+    });
+  }
+  console.log(`Seeded ${users.length} users`);
+}
+
 async function main() {
   try {
-    await seedMovies();
+    // await seedMovies();
+    // await seedShowtimes();
+    await seedUsers();
+    await seedReviews();
   } catch (err) {
     console.error('An error occurred while attempting to seed the database:', err);
   }
